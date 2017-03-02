14759085212fd6650105945ea2e805ae5690c080
David Zhao <davidzhaohan@gmail.com>
2017-03-02 18:47:07 +0800

    correct conversion mistake

--- app/src/main/java/cs4347/group1/fauxstringinstrument/SensorUtil.java
+++ app/src/main/java/cs4347/group1/fauxstringinstrument/SensorUtil.java
@@ -3,7 +3,7 @@ package cs4347.group1.fauxstringinstrument;
 public final class SensorUtil {
 
     public static float degreeFromRadian(float radian) {
-        double degree = -radian * Math.PI / 180.0;
+        double degree = -radian * 180.0 / Math.PI;
         return (float) degree;
     }
 }
