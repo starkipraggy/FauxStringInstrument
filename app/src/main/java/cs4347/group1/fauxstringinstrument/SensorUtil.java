57db24824a9c1e3c9a9d1bf62ae5003a0c42eecd
starkipraggy <starkipraggy@hotmail.com>
2017-04-07 01:03:19 +0800

    Added low-pass and high-pass filter functions to SensorUtil Changed build.gradle to be compatible with Android Studio 2.4

--- app/src/main/java/cs4347/group1/fauxstringinstrument/SensorUtil.java
+++ app/src/main/java/cs4347/group1/fauxstringinstrument/SensorUtil.java
@@ -8,4 +8,35 @@ public final class SensorUtil {
         double degree = -radian * 180.0 / Math.PI;
         return (float) degree;
     }
+
+    /**
+     * Low pass filter helper function. Run on a single float.
+     * @param input The input from the phone sensor
+     * @param prev Result from previous iteration
+     * @param alpha Alpha value to use for calculation (see function notes)
+     * @return The current result
+     */
+    // alpha is calculated as t / (t + dT)
+    // with t, the low-pass filter's time-constant
+    // and dT, the event delivery rate
+    // fiddle around with the alpha value
+    public static float lowPassFilter(float input, float prev, float alpha){
+        return alpha * prev + (1 - alpha) * input;
+    }
+
+    /**
+     * High pass filter helper function. Run on a single float.
+     * @param input The input from the phone sensor
+     * @param prev Result from previous iteration
+     * @param prevInput The preceding input from the phone sensor
+     * @param alpha Alpha value to use for calculation (see function notes)
+     * @return The current result
+     */
+    // alpha is calculated as t / (t + dT)
+    // with t, the low-pass filter's time-constant
+    // and dT, the event delivery rate
+    // fiddle around with the alpha value
+    public static float highPassFilter(float input, float prev, float prevInput, float alpha){
+        return alpha * (prev + input - prevInput);
+    }
 }
