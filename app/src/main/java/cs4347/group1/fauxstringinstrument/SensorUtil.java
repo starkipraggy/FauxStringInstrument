f3edb3fecd980e1182133a1740a9f48e962ce0d7
starkipraggy <starkipraggy@hotmail.com>
2017-04-07 01:17:31 +0800

    Added removeGravity demo function

--- app/src/main/java/cs4347/group1/fauxstringinstrument/SensorUtil.java
+++ app/src/main/java/cs4347/group1/fauxstringinstrument/SensorUtil.java
@@ -3,6 +3,7 @@ package cs4347.group1.fauxstringinstrument;
 public final class SensorUtil {
 
     public static final int SAME_NOTE_COUNT = 1;
+    public static float[] prevGravity = {0.0f, 0.0f, 0.0f};
 
     public static float degreeFromRadian(float radian) {
         double degree = -radian * 180.0 / Math.PI;
@@ -38,5 +39,22 @@ public final class SensorUtil {
     // fiddle around with the alpha value
     public static float highPassFilter(float input, float prev, float prevInput, float alpha){
         return alpha * (prev + input - prevInput);
+    }
+
+    /**
+     * Demonstration on how to remove gravity component as given in Android docs
+     * @param input The sensor data, from 0 to 2
+     * @return Sensor data without gravity component
+     */
+    public static float[] removeGravity(float[] input){
+        float[] result = new float[3];
+        float alpha = 0.8f;
+
+        for (int i = 0; i < 3; i++) {
+            prevGravity[i] = lowPassFilter(input[i], prevGravity[i], alpha);
+            result[i] = input[i] - prevGravity[i];
+        }
+
+        return result;
     }
 }
